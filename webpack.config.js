dad566bb9722505368097c8340b7a656a3af967f
lixiaoqing <lixiaoqing0126@outlook.com>
2020-05-13 19:08:27 +0800

    插件

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,6 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
   mode: 'development',
@@ -7,6 +9,12 @@ module.exports = {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      // template: 'src/index.html'
+    }),
+    new CleanWebpackPlugin()
+  ],
   module: {
     rules: [
       {
