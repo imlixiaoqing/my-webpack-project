52edf0a904b9d3b38aaabc91333b77df3e1d082a
lixiaoqing <lixiaoqing0126@outlook.com>
2020-05-19 21:56:40 +0800

    ing

--- src/index.js
+++ src/index.js
@@ -1,16 +1,16 @@
 import _ from 'lodash';
-import './style.scss';
-import Icon from './icon.jpg';
+import printMe from './print.js';
 
 function component() {
   const element = document.createElement('div');
+  const btn = document.createElement('button');
 
   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-  element.classList.add('hello');
 
-  const myIcon = new Image();
-  myIcon.src = Icon;
-  element.appendChild(myIcon);
+  btn.innerHTML = 'Click me and check the console!';
+  btn.onclick = printMe;
+
+  element.appendChild(btn);
 
   return element;
 }
--- webpack.config.js
+++ webpack.config.js
@@ -3,51 +3,39 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
+
   mode: 'development',
-  entry: './src/index.js',
-  output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist'),
+  // mode: 'production',
+  // devtool: 'cheap-module-source-map',
+
+  entry: {
+    app: './src/index.js',
+    print: './src/print.js'
+  },
+  devtool: 'eval-cheap-module-source-map',
+  devServer: {
+    contentBase:  './dist',
+    // open: true,
+    // port: '8000',
+    // proxy: {
+    //   // 'api': 'http://localhost:3000/'
+    // }
   },
   plugins: [
+    new CleanWebpackPlugin({
+      cleanStaleWebpackAssets: false
+    }),
     new HtmlWebpackPlugin({
+      title: 'Output 122',
       // template: 'src/index.html'
     }),
-    new CleanWebpackPlugin()
   ],
-  module: {
-    rules: [
-      {
-        test: /\.css$/,
-        use: [
-          'style-loader',
-          'css-loader'
-        ],
-      },
-      {
-        test: /\.scss$/,
-        use: [
-          'style-loader',
-          'css-loader',
-          'sass-loader',
-          'postcss-loader'
-        ],
-      },
-      // {
-      //   test: /\.(png|svg|jpg|gif)$/,
-      //   loader: 'url-loader',
-      //   options: {
-      //     limit: 204800
-      //   }
-      // },
-      {
-        test: /\.(png|svg|jpg|gif)$/,
-        loader: 'file-loader',
-        options: {
-          name: '[name].[ext]',
-          outputPath: 'images'
-        }
-      }
-    ]
+
+  
+
+  output: {
+    // publicPath: '/',
+    // filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
   }
 };
